19dadbb6d8546028f9086476ea887be73dea79cc
Patrick Huesler <patrick.huesler@gmail.com>
2011-08-19 18:53:23 +0200

    ported the code. Unsurprisengly, the tests do not pass

--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -6,18 +6,49 @@ var invalidRequest2 = "53umfudisP7mKhsi9nZboBg15yMZKhfQAARL9UoZtSE.eyJhbGdvcml0a
 
 
 var SignedRequest = require('../lib/facebook-signed-request');
-SignedRequest.secret = "897z956a2z7zzzzz5783z458zz3z7556";
 
-describe('Tracking', function(){
+describe('parse signed requests', function(){
+  beforeEach(function(){
+   SignedRequest.secret = "897z956a2z7zzzzz5783z458zz3z7556";
+  });
+
   it('parses a valid request',function(){
     var request = new SignedRequest( validRequest );
-    expect(request.isValid).toBeTruthy();
+    expect(request.isValid()).toBeTruthy();
   });
 
-  it('is not valid for requests with an invalid paylod',function(){
+  it('is not valid for requests with an invalid signature',function(){
     var request = new SignedRequest( invalidRequest1 );
-    expect(request.isValid).toBeFalsey();
+    expect(request.isValid()).toBeFalsy();
+    expect(request.errors.length).toEqual(2);
   });
 
+  it('is not valid for requests with an invalid payload',function(){
+    var request = new SignedRequest( invalidRequest1 );
+    expect(request.isValid()).toBeFalsy();
+    expect(request.errors.length).toEqual(4);
+  });
+
+  it('throws an error when the secret is invalid', function(){
+    var expectedMessage = "Secret should be a String";
+    expect(new SignedRequest( 'foo.bar', { secret : 2 } )).toThrow(expectedMessage);
+  });
+
+  it('throws an error when the secret is missing', function(){
+    var expectedMessage =  "No secret provided. Use SignedRequest.secret= or the options object";
+    SignedRequest.secret = null;
+    expect(new SignedRequest( 'foo.bar' )).toThrow(expectedMessage);
+  });
+
+  it('throws an error when invalid parameters are given', function(){
+    var expectedMessage = "Invalid Format. See http://developers.facebook.com/docs/authentication/signed_request/";
+    expect(new SignedRequest( 'foobar' )).toThrow(exptectedMessage);
+  });
+
+  it('throws an error with scrict : true and expired oauth token', function(){
+    var expectedMessage = "OAuth Token has expired";
+    SignedRequest.secret = null;
+    expect(new SignedRequest( validRequest , { strict : true } )).toThrow(expectedMessage);
+  });
 });
 
