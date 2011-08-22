bbc865e981b03b22e5332ca00125e04a360eacdf
Patrick Huesler <patrick.huesler@gmail.com>
2011-08-22 18:37:10 +0200

    make all the tests pass
    There is still the issue that crypto.createHmac cannot output a string,
    only base64. So we have to decode it. One option would be, to leave out
    the base64 decoding part for the signature and just compare the base64
    versions.

--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -20,35 +20,49 @@ describe('parse signed requests', function(){
   it('is not valid for requests with an invalid signature',function(){
     var request = new SignedRequest( invalidRequest1 );
     expect(request.isValid()).toBeFalsy();
-    expect(request.errors.length).toEqual(2);
+    expect(request.errors.length).toEqual(1);
+    expect(request.errors[0]).toMatch("Signatures did not match");
   });
 
   it('is not valid for requests with an invalid payload',function(){
-    var request = new SignedRequest( invalidRequest1 );
+    var request = new SignedRequest( invalidRequest2 );
     expect(request.isValid()).toBeFalsy();
-    expect(request.errors.length).toEqual(4);
+    expect(request.errors[0]).toMatch("Invalid JSON object");
+    expect(request.errors[1]).toMatch("Invalid Algorithm");
+    expect(request.errors[2]).toMatch("Signatures did not match");
   });
 
   it('throws an error when the secret is invalid', function(){
     var expectedMessage = "Secret should be a String";
-    expect(new SignedRequest( 'foo.bar', { secret : 2 } )).toThrow(expectedMessage);
+    var fn = function(){
+      return new SignedRequest( 'foo.bar', { secret : 2 } );
+    };
+    expect(fn).toThrow(expectedMessage);
   });
 
   it('throws an error when the secret is missing', function(){
     var expectedMessage =  "No secret provided. Use SignedRequest.secret= or the options object";
     SignedRequest.secret = null;
-    expect(new SignedRequest( 'foo.bar' )).toThrow(expectedMessage);
+    var fn = function(){
+      return new SignedRequest( 'foo.bar' );
+    };
+    expect(fn).toThrow(expectedMessage);
   });
 
   it('throws an error when invalid parameters are given', function(){
     var expectedMessage = "Invalid Format. See http://developers.facebook.com/docs/authentication/signed_request/";
-    expect(new SignedRequest( 'foobar' )).toThrow(exptectedMessage);
+    var fn = function(){
+      return new SignedRequest( 'foobar' );
+    };
+    expect(fn).toThrow(expectedMessage);
   });
 
   it('throws an error with scrict : true and expired oauth token', function(){
     var expectedMessage = "OAuth Token has expired";
-    SignedRequest.secret = null;
-    expect(new SignedRequest( validRequest , { strict : true } )).toThrow(expectedMessage);
+    var fn = function(){
+      return new SignedRequest( validRequest , { strict : true } );
+    };
+    expect(fn).toThrow(expectedMessage);
   });
 });
 
