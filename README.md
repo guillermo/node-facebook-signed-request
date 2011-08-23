a40fe8d7baf357b1cc0d42d8512b228f2c115202
Patrick Huesler <patrick.huesler@gmail.com>
2011-08-23 14:57:50 +0200

    changed the API to use callbacks

--- README.md
+++ README.md
@@ -3,31 +3,40 @@
 node.js port of [facebook-signeded-request](https://github.com/wooga/facebook-signed-request).
 It implements the [signed request](http://developers.facebook.com/docs/authentication/signed_request/) sent by facebook to applications.
 
-## Installation
+## Installation and Usage
 
 clone this repository and then
 
 	var SignedRequest = require(PATH_TO_LIB + '/lib/facebook-signed-request');
-  	SignedRequest.secret = "your facebook application secret";
-  	var request = yourRequestObjectParamsHash['signed_request'];
-  	var signedRequest = new SignedRequest( request );
+	SignedRequest.secret = "your facebook application secret";
+	var request = yourRequestObjectParamsHash['signed_request'];
+	var signedRequest = new SignedRequest( request );
 
-  	console.log(signedRequest.isValid());
-  	console.log(signedRequest.errors);
+	signedRequest.parse(function(errors, request){
+	  // check if request was valid
+	  console.log(request.isValid());
+
+	  // access errors
+	  console.log(errors);
+
+	  // this is your data object
+	  console.log(request.data);
+	});
+
+## Data sent by facebook
+
+Here is an example
+
+	{
+		algorithm: 'HMAC-SHA256',
+		expires: 1308988800,
+		issued_at: 1308985018,
+		oauth_token: '111111111111111|2.AQBAttRlLVnwqNPZ.3600.1111111111.1-111111111111111|T49w3BqoZUegypru51Gra70hED8',
+		user: { country: 'de', locale: 'en_US', age: { min: 21 } },
+		user_id: '111111111111111'
+	}
 
 ## Tests
 
 	npm install jasmine-node
 	jasmine-node spec
-
-## Considerdations
-
-The API is still pretty alpha and will probably change with the next release since the Ruby implementation is not a really good fit for JavaScript for the following reasons:
-
-* No callbacks. Some parts are CPU intensive (SHA-256, HMAC, base64) and
-  therefore might block the reactor. A solution with callbacks is
-preferrred (I've started the work on that and it will soon be finished)
-* The exception throwing part: This might be a good idea for (non evented) Ruby but not for node.js, since this can kill the whole node process when the errors are not handled. There is a lot of data transformation going on (sha-256, base64, HMAC, JSON), so errors will be common. I prefer an approach that returns an error in the callback as soon as it has found one.
-* node.js crypto.digest does not return encodings other than binary, hex or base64. This requires to decode the base64 again and compare with facebook's signature. At least that is how facebook does it in their reference implementation. One could try to not decode their signature and compare it directly with the output of crypto.digest. At this point, I don't see why this should not work but I'll have to try it in the wild first
-
-
--- spec/TestSpec.js
+++ spec/TestSpec.js
@@ -13,57 +13,62 @@ describe('parse signed requests', function(){
   });
 
   it('parses a valid request',function(){
-    new SignedRequest( validRequest ).parse(function(request){
+    new SignedRequest( validRequest ).parse(function(errors, request){
       expect(request.isValid()).toBeTruthy();
     });
   });
 
-  //it('is not valid for requests with an invalid signature',function(){
-    //var request = new SignedRequest( invalidRequest1 );
-    //expect(request.isValid()).toBeFalsy();
-    //expect(request.errors.length).toEqual(1);
-    //expect(request.errors[0]).toMatch("Signatures did not match");
-  //});
+  it('is not valid for requests with an invalid signature',function(){
+    new SignedRequest( invalidRequest1 ).parse(function(errors, request){
+      expect(request.isValid()).toBeFalsy();
+      expect(errors[0]).toMatch("Signatures did not match");
+    });
+  });
 
-  //it('is not valid for requests with an invalid payload',function(){
-    //var request = new SignedRequest( invalidRequest2 );
-    //expect(request.isValid()).toBeFalsy();
-    //expect(request.errors[0]).toMatch("Invalid JSON object");
-    //expect(request.errors[1]).toMatch("Invalid Algorithm");
-    //expect(request.errors[2]).toMatch("Signatures did not match");
-  //});
+  it('is not valid for requests with an invalid payload',function(){
+    new SignedRequest( invalidRequest2 ).parse(function(errors, request){
+      expect(errors[0]).toMatch("Invalid JSON object");
+    });
+  });
 
-  //it('throws an error when the secret is invalid', function(){
-    //var expectedMessage = "Secret should be a String";
-    //var fn = function(){
-      //return new SignedRequest( 'foo.bar', { secret : 2 } );
-    //};
-    //expect(fn).toThrow(expectedMessage);
-  //});
+  it('has an error when the secret is invalid', function(){
+    new SignedRequest( 'foo.bar', { secret : 2 } ).parse(function(errors, request){
+      expect(errors[0]).toMatch("Secret should be a String");
+    });
+  });
 
-  //it('throws an error when the secret is missing', function(){
-    //var expectedMessage =  "No secret provided. Use SignedRequest.secret= or the options object";
-    //SignedRequest.secret = null;
-    //var fn = function(){
-      //return new SignedRequest( 'foo.bar' );
-    //};
-    //expect(fn).toThrow(expectedMessage);
-  //});
+  it('has an error when the secret is missing', function(){
+    var expectedMessage =  "No secret provided. Use SignedRequest.secret= or the options object";
+    SignedRequest.secret = null;
+    new SignedRequest( 'foo.bar' ).parse(function(errors, request){
+      expect(errors[0]).toMatch(expectedMessage);
+    });
+  });
 
-  //it('throws an error when invalid parameters are given', function(){
-    //var expectedMessage = "Invalid Format. See http://developers.facebook.com/docs/authentication/signed_request/";
-    //var fn = function(){
-      //return new SignedRequest( 'foobar' );
-    //};
-    //expect(fn).toThrow(expectedMessage);
-  //});
+  it('has an error when invalid parameters are given', function(){
+    var expectedMessage = "Invalid Format. See http://developers.facebook.com/docs/authentication/signed_request/";
+    new SignedRequest( 'foobar' ).parse(function(errors, request){
+      expect(errors[0]).toMatch(expectedMessage);
+    });
+  });
 
-  //it('throws an error with scrict : true and expired oauth token', function(){
-    //var expectedMessage = "OAuth Token has expired";
-    //var fn = function(){
-      //return new SignedRequest( validRequest , { strict : true } );
-    //};
-    //expect(fn).toThrow(expectedMessage);
-  //});
+  it('has an error with scrict : true and expired oauth token', function(){
+    var expectedMessage = "OAuth Token has expired";
+    new SignedRequest( validRequest , { strict : true } ).parse(function(errors, request){
+      expect(errors[0]).toMatch(expectedMessage);
+    });
+  });
+
+  it('provides access to the parsed payload data', function(){
+    new SignedRequest( validRequest , { strict : true } ).parse(function(errors, request){
+      expect(request.data).toBeDefined();
+    });
+  });
+
+  it('provides access to the parsed payload data', function(){
+    new SignedRequest( validRequest , { strict : true } ).parse(function(errors, request){
+      expect(request.data.user_id).toEqual('111111111111111');
+    });
+  });
 });
 
